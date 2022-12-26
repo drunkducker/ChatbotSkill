b1ad2d1abd84a75a50d0bab877d827a0fcbfe6e6
drunkducker <44531325+drunkducker@users.noreply.github.com>
2022-12-26 10:00:06 -0500

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Install ChatBotAI library
+mycroft-pip install ChatBotAI
+
+# Install NLTK library
+mycroft-pip install NLTK
+
+# Install scikit-learn library
+mycroft-pip install scikit-learn
+
+# Install SQLite library
+mycroft-pip install SQLite
